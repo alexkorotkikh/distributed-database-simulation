7cedd79c2d0cdca8b1081254b4ffe0a4caee084c
Alex Korotkikh <a.korotkikh@gmail.com>
2012-02-19 15:04:38 +0200

    statistic tests were fixed

--- tests/test_statistic.py
+++ tests/test_statistic.py
@@ -5,40 +5,40 @@ from statistic import Statistic, State, STAT_STRING
 __author__ = 'akril'
 
 class TestStatistic(TestCase):
-    def setUp(self):
-        self.statistic = Statistic(None)
-
-
     def test_add_req(self):
         #given
         dest = Infocenter(10, 3)
-        request = Request(Node(), dest, 1)
+        self.statistic = Statistic([(Node()), dest])
+
+        request = Request(0, 1, 1)
 
         #when
         self.statistic.add_req(request)
 
         #then
-        self.assertEqual(self.statistic.timeline.state_for(dest, 0), None)
+        self.assertIsNone(self.statistic.timeline.state_for(dest, 0))
 
-        state = self.statistic.timeline.state_for(dest, 1)
+        state = self.statistic.timeline.state_for(1, 1)
         self.assertEqual(state.start, 1)
-        self.assertTrue(7 <= state.end <= 13)
-        self.assertEqual(state.node, dest)
+        self.assertTrue(8 <= state.end <= 14)
+        self.assertEqual(state.node, 1)
 
 
     def test_add_req_with_intersection(self):
-        #given
+        # given
         dest = Infocenter(10, 3)
-        first = Request(Node(), dest, 1)
-        second = Request(Node(), dest, 2)
+        self.statistic = Statistic([Node(), Node(), dest])
 
-        #when
+        first = Request(0, 2, 1)
+        second = Request(1, 2, 2)
+
+        # when
         self.statistic.add_req(first)
         self.statistic.add_req(second)
 
-        #then
-        first_state = self.statistic.timeline.state_for(dest, 1)
-        second_state = self.statistic.timeline.state_for(dest, first_state.end + 1)
+        # then
+        first_state = self.statistic.timeline.state_for(2, 1)
+        second_state = self.statistic.timeline.state_for(2, first_state.end + 1)
 
         self.assertIsNotNone(second_state)
         self.assertEquals(first_state.end + 1, second_state.start)
@@ -46,7 +46,9 @@ class TestStatistic(TestCase):
 
     def test_fill_stat_string(self):
         #given
-        node_id = 3
+        self.statistic = Statistic([Node(), Infocenter(2, 1)])
+
+        node_id = 1
         states = [State(node_id, 3, 8), State(node_id, 9, 15)]
 
         #when
@@ -54,6 +56,6 @@ class TestStatistic(TestCase):
 
         #then
         util = ((8 - 3) + (15 - 9)) / float(100)
-        expected = STAT_STRING.format(node_id=node_id, request_number=2, util=util)
+        expected = STAT_STRING.format(node_id=node_id, type="Infocenter", request_number=2, util=util)
         self.assertEquals(actual, expected)
 
